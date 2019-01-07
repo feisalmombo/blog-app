c7eb816ed38cbb49fbbb9ee1c33ee3ef77d9e904
feisal mombo <feisalmombo29@gmail.com>
2019-01-07 16:50:13 +0300

    initial commit

---
+++ blog/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+
+from .views import BlogListView, BlogDetailView 
+
+
+urlpatterns = [
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), # new
+    path('', BlogListView.as_view(), name='home'),
+]
